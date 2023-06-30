ea62598fdd28f46578fb4d0add64f38e216da8cd
HUNT16 <vivekthakor1690@gmail.com>
2023-06-30 12:47:09 +0500

    change in hooks.py to calling method in jinja

--- saraswati_customization/hooks.py
+++ saraswati_customization/hooks.py
@@ -112,23 +112,23 @@ doc_events = {
 # }
 
 
-# jenv = {
-#         "methods":
-# 		[
-# 			"get_sales_invoice_data:saraswati_customization.saraswati_customization.jinja_function.get_sales_invoice_data.get_sales_invoice_data",
-# 			"get_sales_invoice_data_pricelist:saraswati_customization.saraswati_customization.jinja_function.get_sales_invoice_data_pricelist.get_sales_invoice_data_pricelist",
-# 			"get_delivery_note_data:saraswati_customization.saraswati_customization.jinja_function.get_delivery_note_data.get_delivery_note_data"
-# 		]
-# }
-
-jinja ={
-    "methods":
+jenv = {
+        "methods":
 		[
-			"saraswati_customization.saraswati_customization.jinja_function.get_sales_invoice_data.get_sales_invoice_data",
-			"saraswati_customization.saraswati_customization.jinja_function.get_sales_invoice_data_pricelist.get_sales_invoice_data_pricelist",
-			"saraswati_customization.saraswati_customization.jinja_function.get_delivery_note_data.get_delivery_note_data"
+			"get_sales_invoice_data:saraswati_customization.saraswati_customization.jinja_function.get_sales_invoice_data.get_sales_invoice_data",
+			"get_sales_invoice_data_pricelist:saraswati_customization.saraswati_customization.jinja_function.get_sales_invoice_data_pricelist.get_sales_invoice_data_pricelist",
+			"get_delivery_note_data:saraswati_customization.saraswati_customization.jinja_function.get_delivery_note_data.get_delivery_note_data"
 		]
 }
+
+# jinja ={
+#     "methods":
+# 		[
+# 			"saraswati_customization.saraswati_customization.jinja_function.get_sales_invoice_data.get_sales_invoice_data",
+# 			"saraswati_customization.saraswati_customization.jinja_function.get_sales_invoice_data_pricelist.get_sales_invoice_data_pricelist",
+# 			"saraswati_customization.saraswati_customization.jinja_function.get_delivery_note_data.get_delivery_note_data"
+# 		]
+# }
 # doc_events = {
 #	"*": {
 #		"on_update": "method",
